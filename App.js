f5d3a2cc2a6bb4830ab9bc5cb4e0175b985b1fe3
Divyank jha <jhadivyank@gmail.com>
2023-09-23 15:50:40 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,17 @@
+
+const heading = React.createElement("h1", {id : "heading"}, "Hello world by react!");
+
+// nested React Element
+
+const parent =  React.createElement("div", {id : "parent"}, 
+React.createElement("div", {id : "Child"}, [
+React.createElement("h1", {}, "I am an HTmlTag"),
+React.createElement("h2", {}, "I am an HTmlTag") ]),
+React.createElement("div", {id : "parent"}, 
+React.createElement("div", {id : "Child"}, [
+React.createElement("h1", {}, "I am an HTmlTag"),
+React.createElement("h2", {}, "I am an HTmlTag") ])))
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
